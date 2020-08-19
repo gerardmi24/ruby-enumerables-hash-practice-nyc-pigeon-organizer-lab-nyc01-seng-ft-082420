65cabafb0a1116b51d1253f42f7edf142cc0a8ad
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-19 03:58:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
-
     value.each do |inner_key, names|
       names.each do |name|
-
         if !final_array[name]
           final_array[name] = {}
         end
@@ -15,5 +13,4 @@ def nyc_pigeon_organizer(data)
     end
   end
 end
-
 end
